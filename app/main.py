97caa5c5d0c3d9e50356636fe1e7aa5ca543a0cd
Quentin Bahers <quentin.bahers@gmail.com>
2025-04-22 21:48:56 +0200

    Extract URL path

--- app/main.py
+++ app/main.py
@@ -7,7 +7,14 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
-    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    data = connection.recv(1024).decode("ISO-8859-1").split("\r\n\r\n")
+    request_header = data[0].split("\r\n")
+    request_line = request_header[0].split()
+    request_target = request_line[1]
+    if request_target == "/":
+        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
