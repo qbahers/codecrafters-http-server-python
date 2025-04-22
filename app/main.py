3316ccbbb3d8e65b151a52e2b7e41e1de399088a
Quentin Bahers <quentin.bahers@gmail.com>
2025-04-22 23:42:44 +0200

    Read header

--- app/main.py
+++ app/main.py
@@ -11,11 +11,15 @@ def main():
     request_header = data[0].split("\r\n")
     request_line = request_header[0].split()
     request_target = request_line[1]
+    headers = request_header[1:]
     if request_target == "/":
         connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     elif request_target.startswith("/echo/"):
         string = request_target.split("/")[-1]
         connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode("ISO-8859-1"))
+    elif request_target.startswith("/user-agent"):
+        user_agent = next(x for x in headers if x.lower().startswith("user-agent:")).split(":")[-1].strip()
+        connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode("ISO-8859-1"))
     else:
         connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
