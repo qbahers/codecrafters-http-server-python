552659afbaba2f1c5eb10fa73137c2d9136c7a92
Quentin Bahers <quentin.bahers@gmail.com>
2025-04-25 19:43:51 +0200

    Persistent connection

--- app/main.py
+++ app/main.py
@@ -12,48 +12,50 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
         connection, address = server_socket.accept() # wait for client
-        threading.Thread(target=handle_request, args=(connection,), daemon=True).start()
+        threading.Thread(target=handle_requests, args=(connection,), daemon=True).start()
 
-def handle_request(connection):
-    data = connection.recv(1024).decode("ISO-8859-1").split("\r\n\r\n")
-    request_header = data[0].split("\r\n")
-    request_line = request_header[0].split()
-    http_method = request_line[0]
-    request_target = request_line[1]
-    headers = request_header[1:]
-    request_body = data[1]
-    if request_target == "/":
-        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    elif request_target.startswith("/echo/"):
-        string = request_target.split("/")[-1].encode("ISO-8859-1")
-        accept_encoding = next((x for x in headers if x.lower().startswith("accept-encoding:")), None)
-        content_encoding = "Content-Encoding: gzip\r\n" if accept_encoding and "gzip" in [x.strip() for x in accept_encoding.split(":")[-1].split(",")] else ""
-        if content_encoding != "":
-            string = gzip.compress(string)
-        connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n{content_encoding}\r\n".encode("ISO-8859-1") + string)
-    elif request_target.startswith("/user-agent"):
-        user_agent = next(x for x in headers if x.lower().startswith("user-agent:")).split(":")[-1].strip()
-        connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode("ISO-8859-1"))
-    elif request_target.startswith("/files/"):
-        filename = request_target.split("/")[-1]
-        file = os.path.join(sys.argv[2], filename)
-        if http_method == "GET":
-            try:
-                with open(file) as f:
-                    data = f.read()
-                    connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode("ISO-8859-1"))
-            except:
+def handle_requests(connection):
+    while True:
+        data = connection.recv(1024)
+        if data != b'':
+            data = data.decode("ISO-8859-1").split("\r\n\r\n")
+            request_header = data[0].split("\r\n")
+            request_line = request_header[0].split()
+            http_method = request_line[0]
+            request_target = request_line[1]
+            headers = request_header[1:]
+            request_body = data[1]
+            if request_target == "/":
+                connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            elif request_target.startswith("/echo/"):
+                string = request_target.split("/")[-1].encode("ISO-8859-1")
+                accept_encoding = next((x for x in headers if x.lower().startswith("accept-encoding:")), None)
+                content_encoding = "Content-Encoding: gzip\r\n" if accept_encoding and "gzip" in [x.strip() for x in accept_encoding.split(":")[-1].split(",")] else ""
+                if content_encoding != "":
+                    string = gzip.compress(string)
+                connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n{content_encoding}\r\n".encode("ISO-8859-1") + string)
+            elif request_target.startswith("/user-agent"):
+                user_agent = next(x for x in headers if x.lower().startswith("user-agent:")).split(":")[-1].strip()
+                connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode("ISO-8859-1"))
+            elif request_target.startswith("/files/"):
+                filename = request_target.split("/")[-1]
+                file = os.path.join(sys.argv[2], filename)
+                if http_method == "GET":
+                    try:
+                        with open(file) as f:
+                            data = f.read()
+                            connection.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode("ISO-8859-1"))
+                    except:
+                        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                else:
+                    try:
+                        with open(file, "w") as f:
+                            f.write(request_body)
+                            connection.sendall(b"HTTP/1.1 201 Created\r\n\r\n")
+                    except:
+                        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            else:
                 connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-        else:
-            try:
-                with open(file, "w") as f:
-                    f.write(request_body)
-                    connection.sendall(b"HTTP/1.1 201 Created\r\n\r\n")
-            except:
-                connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-    else:
-        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-    connection.close()
 
 
 if __name__ == "__main__":
